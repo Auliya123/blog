246284147be741f8c1b530c48ca4b6fa553d24e1
Michellead <auliyamichellea@gmail.com>
2021-06-14 11:09:10 +0700

    useReducer

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import indexScreen from "./src/screens/indexScreen";
+import indexScreen from "./src/screens/IndexScreen";
 import { BlogProvider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
